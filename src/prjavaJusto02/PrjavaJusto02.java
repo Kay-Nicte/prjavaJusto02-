58955e9f7e473b7c29940af5e83381f371740b15
Ixabel Justo <miju355@vidalibarraquer.net>
2022-05-10 09:01:28 +0200

    todo

---
+++ src/prjavaJusto02/PrjavaJusto02.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjavaJusto02;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author --> Escriu el teu correu electrònic
+ */
+public class PrjavaJusto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("versió 0.1 del projecte prjavaJusto02"); 	
+        try {
+            InetAddress adreça = InetAddress.getLocalHost();
+            String hostname = adreça.getHostName();
+            System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("E-mail: ixabeljusto@gmail.com");
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+            System.out.println("Creació d'una branca del projecte prjavaCognom02");
+        }
+        catch (IOException e) {
+		System.out.println("Exception occurred");
+        }  
+    }
+}
